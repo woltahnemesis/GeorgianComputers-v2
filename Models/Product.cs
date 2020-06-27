2dff6ae58fdaa40f7c1fe1e2465c9483de7b9722
Rey Walter Magbanua <alucardmagbanua@gmail.com>
2020-06-26 21:51:37 -0400

    Forgot to push the files the other day. Pushing it now

--- Models/Product.cs
+++ Models/Product.cs
@@ -15,12 +15,14 @@ namespace GeorgianComputers.Models
 
         [Key]
         public int ProductId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Product name is required")]
         [StringLength(255)]
         public string Name { get; set; }
         [StringLength(8000)]
         public string Description { get; set; }
         [Column(TypeName = "decimal(10, 2)")]
+        [Range(0, 999999, ErrorMessage = "Price must be between 0 and 999,999")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
         public decimal Price { get; set; }
         [StringLength(255)]
         public string Photo { get; set; }
